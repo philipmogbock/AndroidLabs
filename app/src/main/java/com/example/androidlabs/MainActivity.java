ff2d50c2dd3e9887e97947023e51c4d42657dc27
Philip Mogbock <mogbock.philip@gmail.com>
2020-06-03 11:51:23 -0400

    step3

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_grid);
+        setContentView(R.layout.activity_main_relative);
 
         //messages
         String toastMessage = MainActivity.this.getResources().getString(R.string.toast_message);
